cf6f63ab065525a5c83702f57301b62fc5c39d8e
David Sensendorf <david@sensendorf.com>
2023-02-25 20:58:39 -0800

    sql work

--- database/bikemaintenance_db.sql
+++ database/bikemaintenance_db.sql
@@ -114,22 +114,124 @@ CREATE TABLE chainring (
     CONSTRAINT FK_cables_bicycle FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
 );
 
-COMMIT;
+CREATE TABLE custom_part (
+    custom_part_id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_custom_part PRIMARY KEY (custom_part_id),
+    CONSTRAINT FK_custom_part_bicycle FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
 
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
 
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
 
-INSERT INTO quiz (quiz_name) VALUES ('test_quiz');
---Question 1
-INSERT INTO question (question_text, correct_answer) VALUES ('What color is the sky?', 2);
-INSERT INTO answer (question_id, answer_number, answer_text)
-VALUES (lastval(), 1, 'yellow'), (lastval(), 2, 'blue'), (lastval(), 3, 'green'), (lastval(), 4, 'red');
-INSERT INTO quiz_question (quiz_id, question_id, question_number)
-VALUES ((SELECT quiz_id FROM quiz WHERE quiz_name = 'test_quiz'), lastval(), 1);
---Question 2
-INSERT INTO question (question_text, correct_answer) VALUES ('A skeleton walks into a bar, and says to the bartender, "Give me a beer and a ..."', 3);
-INSERT INTO answer (question_id, answer_number, answer_text)
-VALUES (lastval(), 1, 'hair brush'), (lastval(), 2, 'dry erase marker'), (lastval(), 3, 'mop'), (lastval(), 4, 'yo-yo');
-INSERT INTO quiz_question (quiz_id, question_id, question_number)
-VALUES ((SELECT quiz_id FROM quiz WHERE quiz_name = 'test_quiz'), lastval(), 2);
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
+
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
+
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
+
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
+
+CREATE TABLE  (
+    _id int GENERATED ALWAYS AS IDENTITY,
+    bicycle_id int REFERENCES bicycle(bicycle_id),
+    name varchar(100) NOT NULL,
+    install_date DATE,
+    distance_in_meters int,
+    age_interval int,
+    distance_interval int,
+    manufacturer varchar(50),
+    model varchar(50),
+    manufacture_year int,
+    CONSTRAINT PK_ PRIMARY KEY (_id),
+    CONSTRAINT FK_ FOREIGN KEY (bicycle_id) REFERENCES bicycle(bicycle_id)
+);
 
 COMMIT;
